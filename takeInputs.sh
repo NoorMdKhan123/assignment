d85740f92b9d76a65a001c50ed4a0d541040e410
Noor123 <noormd6400@gmail.com>
2021-08-16 15:41:33 +0530

    Done with Operation 5th

--- takeInputs.sh
+++ takeInputs.sh
@@ -2,12 +2,13 @@
  read -p "Please enter 1st number:" a 
  read -p "Please enter 2nd number:" b 
  read -p "Please enter 3rd number:" c
+declare -A res
 result1=$(( a + (b * c) ))
-
+res["op1"]=$result1
 result2=$(( (a*b) + c ))
-
+res["op2"]=$result2
 result3=$(( c + (a/b) ))
-
+res["op3"]=$result3
 result4=$(( (a%b) + c))
-
-echo $result1 $result2 $result3 $result4
+res["op4"]=$result4
+echo ${res[*]}
