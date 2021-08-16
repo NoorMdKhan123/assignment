ed350d5cccd323b2ad0cc2a7ae482df630554010
Noor123 <noormd6400@gmail.com>
2021-08-16 13:04:45 +0530

    Done with 2nd Arithmetic operation

--- takeInputs.sh
+++ takeInputs.sh
@@ -2,5 +2,6 @@
  read -p "Please enter 1st numbers:" a 
  read -p "Please enter 2nd numbers:" b 
  read -p "Please enter 3rd numbers:" c
-result=$(( (a*b) + c ))
-echo $result
+result1=$(( a + (b * c) ))
+result2=$(( (a*b) + c ))
+echo $result1 $result2
